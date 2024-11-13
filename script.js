dede9a6c9dd008482d8c55d4b6218f04651ad25c
Mylo <36931363+gitmylo@users.noreply.github.com>
2024-11-13 11:24:23 +0100

    Basic beginning

--- script.js
+++ script.js
@@ -1,88 +1,9 @@
-const lines = [
-    'Writing mobile apps and games',
-    'Creating new experiences',
-    'Working with Artificial Intelligence',
-    'Developing mobile and desktop apps',
-    'Pushing the boundaries of what is possible'
-]
+function homeButtonAction(force = null) {
+    const homemenu = document.getElementById('homemenu')
 
-let selectedIndex = 0
-randomIndex()
-
-let delay = 0
-
-function randomIndex() {
-    selectedIndex = Math.floor(Math.random() * (lines.length + 1))
-}
-
-function getTargetText() {
-    selectedIndex = selectedIndex % lines.length
-    return lines[selectedIndex]
-}
-
-function getLeftValue(textElement) {
-    return textElement.offsetLeft + textElement.offsetWidth
-}
-
-/**
- * Remove the last character from a string.
- * @param input
- * @returns {(string|boolean)[]} [string, done]
- */
-function stripLast(input) {
-    const length = input.length
-    if (length === 0) return ["", true]
-    return [input.substring(0, length - 1), length - 1 === 0]
-}
-
-/**
- * Add the next character of target to a string.
- * @param currentText
- * @param targetText
- * @returns {(string|boolean)[]|(*|boolean)[]} [string, done]
- */
-function addNext(currentText, targetText) {
-    const length = currentText.length
-    const targetLength = targetText.length
-    if (length === targetLength) return [currentText, true]
-    return [currentText + targetText.substring(length, length + 1), length + 1 === targetLength]
-}
-
-function step(textElement) {
-    const currentText = textElement.innerHTML
-    const target = getTargetText()
-    if (target.startsWith(currentText)) {
-        const [newText, done] = addNext(currentText, target)
-        const updated = textElement.innerHTML !== newText
-        if (updated) {
-            delay = 100
-        }
-        textElement.innerHTML = newText
-        delay--
-        if (done && delay === 0) {
-            // selectedIndex++
-            const oldIndex = selectedIndex
-            do {
-                randomIndex()
-            }
-            while (selectedIndex === oldIndex)
-        }
+    if (force != null) {
+        homemenu.classList.toggle("hidden", force)
+        return
     }
-    else {
-        textElement.innerHTML = stripLast(currentText)[0]
-    }
+    homemenu.classList.toggle("hidden")
 }
-
-// Wait for page load before activating typing animation
-document.addEventListener('DOMContentLoaded', function(event) {
-    const animatedDescription = document.getElementById('animated-content')
-    const indicator = document.getElementById('indicator')
-
-    function stepType() {
-        step(animatedDescription)
-
-        indicator.style.left = `${getLeftValue(animatedDescription)}px`
-    }
-
-    setInterval(stepType, 30)
-});
---
+++ todo.md
@@ -0,0 +1,8 @@
+# Important
+* Copyright notice
+* Link to privacy policies
+* Refund policy (not important yet as no purchases available)
+
+# Contact
+* mylo@mylosoftworks.com
+* possibly other ways of contact if able
